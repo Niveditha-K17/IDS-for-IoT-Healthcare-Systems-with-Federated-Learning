0b7c6382dc29ac7de9a9bd7eef8bf9cba6f9a01b
snigdha1012 <76674154+snigdha1012@users.noreply.github.com>
2023-03-26 01:28:37 +0530

    Add files via upload

---
+++ Federated learning/server.py
@@ -0,0 +1,60 @@
+import flwr as fl
+
+import importlib
+
+import utils
+from sklearn.metrics import log_loss
+from sklearn.linear_model import LogisticRegression
+from typing import Dict
+
+import argparse
+
+DEFAULT_SERVER_ADDRESS = "localhost:9090"
+
+
+def get_argument():
+    # Get the argument from terminal
+    parser = argparse.ArgumentParser(description="Flower")
+
+    parser.add_argument(
+        "--optimizer",
+        type=str,
+        default="fedavg",
+        help=f"choose between fedavg and fed+ (default: fedavg)",
+    )
+
+    parser.add_argument(
+        "--server_address",
+        type=str,
+        default=DEFAULT_SERVER_ADDRESS,
+        help=f"gRPC server address (default: {DEFAULT_SERVER_ADDRESS})",
+    )
+
+    parser.add_argument(
+        "--rounds",
+        type=int,
+        default=1,
+        help="Number of rounds (default: 5)",
+    )
+
+    parser.add_argument(
+        "--min_num_clients",
+        type=int,
+        default=2,
+        help="Minimum number of available clients required for sampling (default: 2)",
+    )
+
+    args = parser.parse_args()
+    dict_args = vars(args)
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_argument()
+
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=1.0,  # Sample 100% of available clients for training
+    min_fit_clients=1
+    )
+    fl.server.start_server(config={"num_rounds": 1}, strategy=strategy)
